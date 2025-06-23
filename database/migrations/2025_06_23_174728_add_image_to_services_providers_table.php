b934b1a820f0687e26ebb61da5677e9c4e13294c
MOSABcod <mosababuhazeem4@gmail.com>
2025-06-23 21:00:34 +0300

    dd

--- database/migrations/2025_06_23_174728_add_image_to_services_providers_table.php
+++ database/migrations/2025_06_23_174728_add_image_to_services_providers_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('services_providers', function (Blueprint $table) {
+        Schema::table('service_providers', function (Blueprint $table) {
             $table->string('image')->nullable();
         });
     }
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         
-        Schema::table('services_providers', function (Blueprint $table) {
+        Schema::table('service_providers', function (Blueprint $table) {
             $table->dropColumn('image');
         });
     }
